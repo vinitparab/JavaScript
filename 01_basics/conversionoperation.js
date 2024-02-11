0b763c35a27a51d7a29854c12fe51ebbda151ff6
vinitparab <159689668+vinitparab@users.noreply.github.com>
2024-02-11 15:19:55 +0000

    conversion of datatypes

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,25 @@
+//let score = "vinit"
+/*
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+//"33"=> 33
+//"33abc"=>NaN
+//true=> 1
+//false=>0
+//let isloggedIn = 1
+
+//let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+//""=> false
+//"vinit"=>true
+
+let num = 33
+let stringnumber = Boolean(num)
+console.log(stringnumber);
+console.log(typeof stringnumber);
