2ceba91808d9bb2981f45d4be6140670f33f77e9
vinitparab <159689668+vinitparab@users.noreply.github.com>
2024-02-11 14:34:58 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello")//we are using nodejs not browser
+
+console.log(3+3);
+
+console.log("Vinit");
+let name="vinit"
+let age=18
+let isloggedIn=false
+
+//number=> 2 to the power 53
+//bigint
+//string=> " "
+//boolean=> true/false
+//null=> standalone value
+//undefined =>eg let state;
+//symbol =>unique
+
+//object 
+
+console.log(typeof "Vinit");
+console.log(typeof null);//output-object
+console.log(typeof undefined);
