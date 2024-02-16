e7bb721ca25de51578a26bcf88c0ac9cd0dfc2db
vinitparab <159689668+vinitparab@users.noreply.github.com>
2024-02-16 16:30:50 +0000

    summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,21 @@
+// Primitive
+
+// 7 types : string , Number , Boolean , undefined , null, symbol , Bigint
+
+//Reference(Non primitive)
+// Array , Objects , Functions
+
+//javascript is Dynamically typed language
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id == anotherId);
+
+//Array
+const heros = ["shaktiman","naagraj","doga"];
+
+//object
+let myobj={
+    name: "hitesh",
+    age = 22,
+}
