f3f30126eb8d90720c80a389468db47a46d77026
vinitparab <159689668+vinitparab@users.noreply.github.com>
2024-02-16 13:07:06 +0000

    conversion

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -1,14 +1,14 @@
-//let score = "vinit"
-/*
+let score = "vinit"
+
 console.log(typeof score);
 console.log(typeof (score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
-*/
+
 //"33"=> 33
-//"33abc"=>NaN
+//"33abc"=>NaN-Not a number
 //true=> 1
 //false=>0
 //let isloggedIn = 1
